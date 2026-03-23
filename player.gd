50239b3695c4ef42d6fb5b59352d85a13fbbf2a5
Sudharsshan <sudharsshansy@gmail.com>
2026-03-23 23:03:09 +0530

    added up and down motion animations for the character

--- player.gd
+++ player.gd
@@ -2,3 +2,34 @@ extends Area2D
 
 @export var speed  = 400 # a var to track how fast the player moves (p/s)
 var screen_size # Size of the game window.
+
+func _ready():
+	screen_size = get_viewport_rect().size # fetch the viewport / screen size
+
+func _process(delta):
+	var velocity = Vector2.ZERO # This is the player's mvoement vector.
+	if (Input.is_action_pressed("move_right")):
+		velocity.x += 1
+	if (Input.is_action_pressed("move_left")):
+		velocity.x -= 1
+	if (Input.is_action_pressed("move_up")):
+		velocity.y -= 1
+	if (Input.is_action_pressed("move_down")):
+		velocity.y += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
